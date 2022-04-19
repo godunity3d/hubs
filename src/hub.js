065b77997395817a5410c4ef4bc733e2a17dde55
godunity3d <godunity3d@gmail.com>
2022-04-19 17:13:53 +0530

    Three sixty image switch implement

--- src/hub.js
+++ src/hub.js
@@ -146,6 +146,7 @@ import "./components/optional-alternative-to-not-hide";
 import "./components/avatar-audio-source";
 import "./components/avatar-inspect-collider";
 import "./components/video-texture-target";
+import "./components/three-sixty-image-switch";
 
 import ReactDOM from "react-dom";
 import React from "react";
